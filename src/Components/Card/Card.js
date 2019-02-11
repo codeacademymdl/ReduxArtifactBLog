40b3bb2ed4fd5a865ff892bf17f7451aebbaac53
Hemant <hhsriteja@gmail.com>
2019-02-11 10:42:58 +0530

    made the website responsive and added flex layout

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -8,27 +8,16 @@ import ClappingImage from '../../Icons/clapping.svg';
 class Card extends Component {
     render() {
         return (
-            <div className="main" >
-                <div className="backgroundImage" style={{ backgroundImage: `url(${this.props.imageSrc})` }} />
-
-                <div className="imageInfo">
-
-                    <span>{this.props.date}</span><span>{this.props.readingTime}</span>
-                </div>
-                <div className="imageDesc">
-                    <span >{this.props.title}</span>
-                    <span className="imageContent">{this.props.description}</span>
-
-                </div>
-                <div className="imageFooter">
-                    <div className="reaction">
-                        <img src={ClappingImage} />
-                        <span>{this.props.claps}</span>
-                    </div>
-                    {
-                        this.props.liked?<img src={RedHeartImage} /> :<img src={BlackHeartImage} />   
-                    } 
-
+            <div class="card">
+                <img src= {this.props.imageSrc} alt="abstract" className='image' />
+                <div className="container">
+                    <span className="date">{this.props.date}</span>
+                    <span className='date' id="readTime">{this.props.readingTime}</span>
+                    <p className='title'>{this.props.title}</p>
+                    <p className='abstract'>{this.props.description}</p>
+                    <hr />
+                    <img alt="clap" src={ClappingImage} className='clap' />
+                    <img alt="heart" src={RedHeartImage} className='heart' />
                 </div>
             </div>
         );
--- src/Components/CardGrid/CardGrid.js
+++ src/Components/CardGrid/CardGrid.js
@@ -5,23 +5,21 @@ import Card from '../Card/Card'
 class CardGrid extends Component {
   render() {
     return (
-        <div className="card_grid">
-             {
-          this.props.cards.map((card,index)=>{
-              return <Card 
-              date={card.date} 
-              readingTime={card.readingTime}
-              title={card.title}
-              description={card.description}
-              claps={card.claps}
-              liked={card.liked}
-              imageSrc={`./Images/${card.image}`}
-               ></Card>
+      <div className="card_grid">
+        {
+          this.props.cards.map((card, index) => {
+            return <Card
+              date = {card.date}
+              readingTime = {card.readingTime}
+              title = {card.title}
+              description = {card.description}
+              claps = {card.claps}
+              liked = {card.liked}
+              imageSrc = {`./Images/${card.image}`}
+            ></Card>
           })
-
-
-      }
-        </div>
+        }
+      </div>
     );
   }
 }
